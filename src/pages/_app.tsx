c3ce711ff0f2618989dac999cf6721f30fb52092
Aleix <nigeon@gmail.com>
2023-05-03 13:45:32 +0200

    CSP Voting using ui-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
+import { ClientProvider } from '@vocdoni/react-components';
+import { EnvOptions } from '@vocdoni/sdk';
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { ethers } from 'ethers';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const wallet: any = ethers.Wallet.createRandom();
+
   return (
-    <Component {...pageProps} />
+    <ChakraProvider>
+      <ClientProvider env={"dev" as EnvOptions} signer={wallet}>
+        <Component {...pageProps} />
+      </ClientProvider>
+    </ChakraProvider>
   );
 }
